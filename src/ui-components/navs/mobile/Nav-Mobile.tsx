6594fa0e1a80fda2f12ddf21b993a56734ec288d
frank gimeno <frankgimeno3@gmail.com>
2024-10-14 11:06:10 +0200

    .env y estructura navs

---
+++ src/ui-components/navs/mobile/Nav-Mobile.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface NavMobileProps {
+  
+}
+
+const NavMobile: FC<NavMobileProps> = ({ }) => {
+  return (
+    <div>NavMobile</div>
+  );
+};
+
+export default NavMobile;
---
+++ src/ui-components/navs/web/Nav-Web.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface NavWebProps {
+  
+}
+
+const NavWeb: FC<NavWebProps> = ({ }) => {
+  return (
+    <div>NavWeb</div>
+  );
+};
+
+export default NavWeb;

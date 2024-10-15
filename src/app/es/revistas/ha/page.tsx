edf0a02aae4b3127ef734b850e68ba32599ff78e
frank gimeno <frankgimeno3@gmail.com>
2024-10-16 00:26:15 +0200

    añado contenido principal a revistas

---
+++ src/app/es/revistas/ha/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface HuecoArquitecturaProps {
+  
+}
+
+const HuecoArquitectura: FC<HuecoArquitecturaProps> = ({ }) => {
+  return (
+    <div>HuecoArquitectura</div>
+  );
+};
+
+export default HuecoArquitectura;

83d7506fa9defc66c39bac7d8f863294e4d9658d
frank gimeno <frankgimeno3@gmail.com>
2024-10-15 20:22:53 +0200

    searchbar mejorado

---
+++ src/app/ui-components/navs/SearchBar.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+
+interface SearchBarProps { }
+
+const SearchBar: FC<SearchBarProps> = () => {
+  return (
+    <div className='flex flex-row w-96 border border-white rounded-lg text-white '>
+            <input
+        className='bg-transparent pl-5 py-2 w-full placeholder-white  h-12'
+        placeholder='Buscar...'
+      />
+      <div className='w-20  w-full my-auto'>
+        <svg
+          viewBox="0 0 60 30"
+          xmlns="http://www.w3.org/2000/svg"
+          className=" fill-gray-300 hover:fill-white"
+        >
+          <path d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z" />
+        </svg>
+      </div>
+
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/app/ui-components/navs/web/TopNav.tsx
+++ src/app/ui-components/navs/web/TopNav.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { FC, useState, useEffect } from 'react';
 import Logo from '../../logo/Logo';
+ import SearchBar from '../SearchBar';
 
 interface NavWebProps {}
 
@@ -57,16 +58,9 @@ const NavWeb: FC<NavWebProps> = () => {
                     </div>
                 )}
             </div>
-            <div className='relative w-96'>
-                <svg className='absolute left-3 top-2.5 w-5 h-5 text-white' fill='currentColor' viewBox='0 0 24 24'>
-                    <path d='M10 2a8 8 0 105.293 14.707l5 5a1 1 0 001.414-1.414l-5-5A8 8 0 0010 2zm0 2a6 6 0 110 12A6 6 0 0110 4z' />
-                </svg>
-                <input
-                    className='bg-transparent border border-white rounded-lg text-white pl-10 py-2 w-full placeholder-white'
-                    placeholder='Buscar...'
-                />
-            </div>
-            <div className='flex flex-col'>
+     
+                <SearchBar/>
+             <div className='flex flex-col'>
                 <button className='px-7 py-1 bg-transparent hover:bg-white hover:bg-opacity-10 text-white border border-white rounded-lg my-1'>Registro</button>
                 <button className='px-7 py-1 bg-transparent hover:bg-white hover:bg-opacity-10 text-white border border-white rounded-lg my-1'>Iniciar sesión</button>
             </div>

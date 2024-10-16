f3ca9f4ebbccaacceceaffecdb4dd63453cc3e4b
frank gimeno <frankgimeno3@gmail.com>
2024-10-16 05:19:28 +0200

    reestilo login y signup

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -4,86 +4,105 @@ import { useFormState, useFormStatus } from "react-dom";
 import { handleSignIn } from "@/actions/cognitoActions";
 import Link from "next/link";
 import { Superbutton } from "@/app/ui-components/auth/Superbutton";
- 
+import Logo from "@/app/ui-components/logo/Logo";
+
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(handleSignIn, undefined);
+  const videoUrl = 'https://vidrioperfil-main-bucket.s3.eu-west-3.amazonaws.com/general/videos/perfiles.mp4'
+
+
   return (
-    <form action={dispatch} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={` mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
+    <div className='relative'>
+      <video
+        className="fixed top-0 left-0 object-cover h-screen w-screen bg-black opacity-90 overflow-hidden"
+        src={videoUrl}
+        autoPlay
+        loop
+        muted
+      ></video>
+      <form action={dispatch} className="relative text-white mx-auto h-full " style={{ width: '700px' }}>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-8   h-screen   bg-zinc-900 bg-opacity-50" >
+          <div className="mx-auto "  >
+            <Logo />
+          </div>
+          <h1 className=' my-3  text-2xl text-center'>
+            Iniciar Sesión
+          </h1>
+          <div className="w-full mx-auto"  style={{ width: '500px' }}>
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-white"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Introduzca su correo electrónico"
+                  required
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-white"
+                htmlFor="password"
+              >
+                Contraseña
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Introduzca su contraseña"
+                  required
+                  minLength={6}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mx-auto my-5">
+          <LoginButton />
+          </div>
+          <div className="flex justify-center">
+            <Link
+              href="/auth/reset-password/submit"
+              className="mt-2 cursor-pointer text-blue-300 hover:text-white"
             >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-             </div>
+              Olvidó su contraseña? Haga click aquí.
+            </Link>
           </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
+          <div className="flex justify-center">
+            <Link
+              href="/auth/signup"
+              className="mt-2 cursor-pointer text-blue-300 hover:text-white"
             >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-             </div>
+              {"No dispone de una cuenta? "} Crear una cuenta.
+            </Link>
+          </div>
+          <div className="flex h-8 items-end space-x-1">
+            <div
+              className="flex h-8 items-end space-x-1"
+              aria-live="polite"
+              aria-atomic="true"
+            >
+              {errorMessage && (
+                <>
+                  <p className="text-sm text-red-500">{errorMessage}</p>
+                </>
+              )}
+            </div>
           </div>
         </div>
-        <LoginButton />
-        <div className="flex justify-center">
-          <Link
-            href="/auth/reset-password/submit"
-            className="mt-2 cursor-pointer text-blue-500"
-          >
-            Forgot password? Click here.
-          </Link>
-        </div>
-        <div className="flex justify-center">
-          <Link
-            href="/auth/signup"
-            className="mt-2 cursor-pointer text-blue-500"
-          >
-            {"Don't have an account? "} Sign up.
-          </Link>
-        </div>
-        <div className="flex h-8 items-end space-x-1">
-          <div
-            className="flex h-8 items-end space-x-1"
-            aria-live="polite"
-            aria-atomic="true"
-          >
-            {errorMessage && (
-              <>
-                 <p className="text-sm text-red-500">{errorMessage}</p>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
+
   );
 }
 
@@ -91,8 +110,8 @@ function LoginButton() {
   const { pending } = useFormStatus();
 
   return (
-    <Superbutton className="mt-4 w-full" aria-disabled={pending}>
-      Log in  
+    <Superbutton className="mt-4 text-center" style={{width:'115px'}} aria-disabled={pending}>
+      Iniciar sesión
     </Superbutton>
   );
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,100 +1,101 @@
 "use client";
- 
- import { useFormState, useFormStatus } from "react-dom";
+
+import { useFormState, useFormStatus } from "react-dom";
 import { handleSignUp } from "@/actions/cognitoActions";
 import Link from "next/link";
 import { Superbutton } from "@/app/ui-components/auth/Superbutton";
- 
+import Logo from "@/app/ui-components/logo/Logo";
+
 export default function SignUpForm() {
   const [errorMessage, dispatch] = useFormState(handleSignUp, undefined);
+  const videoUrl = 'https://vidrioperfil-main-bucket.s3.eu-west-3.amazonaws.com/general/videos/perfiles.mp4'
+
   return (
-    <form action={dispatch} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={` mb-3 text-2xl`}>
-          Please create an account.
-        </h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="name"
-                type="text"
-                name="name"
-                minLength={4}
-                placeholder="Enter your name"
-                required
-              />
-             </div>
+    <div className='relative'>
+      <video
+        className="fixed top-0 left-0 object-cover h-screen w-screen bg-black opacity-90 overflow-hidden"
+        src={videoUrl}
+        autoPlay
+        loop
+        muted
+      ></video>
+      <form action={dispatch} className="relative text-white mx-auto h-full " style={{ width: '700px' }}>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-8   h-screen   bg-zinc-900 bg-opacity-50" >
+          <div className="mx-auto "  >
+            <Logo />
           </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-             </div>
+          <h1 className=' my-3   text-2xl text-center'>
+            Crear una cuenta
+          </h1>
+          <div className="w-full mx-auto " style={{ width: '500px' }}>
+ 
+            <div className="mt-4 ">
+              <label
+                className="mt-10 text-center text-sm   leading-9 tracking-tight text-white"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Introduzca un correo electrónico"
+                  required
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                className="mt-10 text-center text-sm leading-9 tracking-tight text-white"
+                htmlFor="password"
+              >
+                Contraseña
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Introduzca una contraseña de 6 o más caracteres"
+                  required
+                  minLength={6}
+                />
+              </div>
+            </div>
           </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-             </div>
+          <div className="mx-auto my-5">
+          <SignUpButton />
           </div>
-        </div>
-        <SignUpButton />
-        <div className="flex justify-center">
-          <Link
-            href="/auth/login"
-            className="mt-2 cursor-pointer text-blue-500"
-          >
-            Already have an account? Log in.
-          </Link>
-        </div>
-        <div className="flex h-8 items-end space-x-1">
-          <div
-            className="flex h-8 items-end space-x-1"
-            aria-live="polite"
-            aria-atomic="true"
-          >
-            {errorMessage && (
-              <>
-                 <p className="text-sm text-red-500">{errorMessage}</p>
-              </>
-            )}
+          <div className=" mx-auto justify-center"  style={{ width: '500px' }}>
+            <div className="flex justify-center">
+              <Link
+                href="/auth/login"
+                className="mt-3 cursor-pointer text-blue-300 hover:text-white"
+              >
+                Ya tiene una cuenta? Inicie sesión.
+              </Link>
+            </div>
+            <div className="flex h-8 items-end space-x-1">
+              <div
+                className="flex h-8 items-end space-x-1"
+                aria-live="polite"
+                aria-atomic="true"
+              >
+                {errorMessage && (
+                  <>
+                    <p className="text-sm text-red-500">{errorMessage}</p>
+                  </>
+                )}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
@@ -103,7 +104,7 @@ function SignUpButton() {
 
   return (
     <Superbutton className="mt-4 w-full" aria-disabled={pending}>
-      Create account  
+      Crear una cuenta
     </Superbutton>
   );
 }

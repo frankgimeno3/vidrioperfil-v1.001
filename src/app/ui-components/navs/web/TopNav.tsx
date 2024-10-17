5054b385e79d81e2d5890570a41904763d6234a2
frank gimeno <frankgimeno3@gmail.com>
2024-10-17 17:47:13 +0200

    cerrar sesion perfect, ediciones mucho más

--- src/app/ui-components/navs/web/TopNav.tsx
+++ src/app/ui-components/navs/web/TopNav.tsx
@@ -23,12 +23,17 @@ const NavWeb: FC<NavWebProps> = ({isLoggedIn}) => {
         
         if (['es', 'al', 'pt'].includes(firstPart)) {
             setEdition(firstPart);
-        }
+        } else {setEdition('es')}
     }, []);  
 
     const changeEdition = (chosenEdition: string) => {
         setEdition(chosenEdition);
     };
+
+    const handleCerrarSesion = ()=>{
+        handleSignOut()
+        window.location.href = '/';
+    }
 
     return (
         <div className='flex flex-row items-center justify-between w-full px-12 text-gray-300' style={{ backgroundColor: '#1d305e' }}>
@@ -36,30 +41,42 @@ const NavWeb: FC<NavWebProps> = ({isLoggedIn}) => {
                 <div className='py-auto' onClick={() => redirect('home')}>
                     <Logo />
                 </div>
-                {/* Edición Links */}
-                <div className='flex flex-col ml-12 w-56 mt-2'>
-                    {edition === '' || edition === undefined ? (
-                        <>
-                            <a href="/es" className='hover:font-bold ' onClick={() => changeEdition('es')}>Edición España</a>
-                            <a href="/pt" className='hover:font-bold' onClick={() => changeEdition('pt')}>Edición Portugal</a>
-                            <a href="/al" className='hover:font-bold' onClick={() => changeEdition('al')}>Edición América Latina</a>
-                        </>
-                    ) : (
-                        <>
-                            {edition === 'es' && <a href="/es" className='hover:font-bold font-bold'>Edición España</a>}
-                            {edition === 'pt' && <a href="/pt" className='hover:font-bold font-bold'>Edição Portugal</a>}
-                            {edition === 'al' && <a href="/al" className='hover:font-bold font-bold'>Edición América Latina</a>}
-                        </>
-                    )}
-                </div>
+                {(edition === 'es') && (
+                    <div className='flex flex-col ml-12 w-56 mt-2'>
+                        <a href="/es" className='hover:font-bold font-bold' onClick={() => changeEdition('es')}>Edición España</a>
+                        <a href="/pt" className='hover:font-bold ' onClick={() => changeEdition('pt')}>Edição Portugal</a>
+                        <a href="/al" className='hover:font-bold' onClick={() => changeEdition('al')}>Edición América Latina</a>
+                    </div>
+                )}
+                {(edition === 'al') && (
+                    <div className='flex flex-col ml-12 w-56 mt-2'>
+                        <a href="/es" className='hover:font-bold' onClick={() => changeEdition('es')}>Edición España</a>
+                        <a href="/pt" className='hover:font-bold ' onClick={() => changeEdition('pt')}>Edição Portugal</a>
+                        <a href="/al" className='hover:font-bold font-bold' onClick={() => changeEdition('al')}>Edición América Latina</a>
+                    </div>
+                )}
+                {(edition === 'pt') && (
+                    <div className='flex flex-col ml-12 w-56 mt-2'>
+                        <a href="/es" className='hover:font-bold' onClick={() => changeEdition('es')}>Edición España</a>
+                        <a href="/pt" className='hover:font-bold font-bold' onClick={() => changeEdition('pt')}>Edição Portugal</a>
+                        <a href="/al" className='hover:font-bold' onClick={() => changeEdition('al')}>Edición América Latina</a>
+                    </div>
+                )}
+                {(edition === '') && (
+                    <div className='flex flex-col ml-12 w-56 mt-2'>
+                        <a href="/es" className='hover:font-bold' onClick={() => changeEdition('es')}>Edición España</a>
+                        <a href="/pt" className='hover:font-bold ' onClick={() => changeEdition('pt')}>Edição Portugal</a>
+                        <a href="/al" className='hover:font-bold' onClick={() => changeEdition('al')}>Edición América Latina</a>
+                    </div>
+                )}
             </div>
             <div className='flex flex-row items-center'>
                 <SearchBar color={'white'} lupaColor={'gray'} placeholderText={'Buscar...'} />
                 <div className='flex flex-row text-xs '>
                     {isLoggedIn ? (
                         <button className='px-5 py-2 bg-red-500 hover:bg-red-700 text-white rounded-lg mr-1'
-                            onClick={handleSignOut}>
-                            Sign Out
+                            onClick={()=>handleCerrarSesion()}>
+                            Cerrar Sesión
                         </button>
                     ) : (
                         <>

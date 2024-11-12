b916ce10629c09632ac756c514511cf86b7db330
frank gimeno <frankgimeno3@gmail.com>
2024-11-12 18:00:16 +0100

    creo interfaces para todos los tipos de usuario

---
+++ src/app/0-components/interfaces/userTypes.ts
@@ -0,0 +1,56 @@
+export interface UserStructure {
+  userId: string;
+  AWSDataType: string;
+  userEmail: string;
+  isModerator: boolean;
+  personalAccountId: string;
+  companyAccounts: CompanyAccountsElement[];  
+}
+
+export interface CompanyAccountsElement {
+  companyAccountId: string;
+  rolesArray: string[];
+}
+
+export interface personalAccount{
+  personalAccountId:string;
+  userId:string;
+  userName:string;
+  userImage:string;
+  userSurnames:string;
+  userCountry:string;
+  userContactEmail:string;
+  userContactPhone:string;
+  userLinkedinLink:string;
+  userWebsiteLink:string;
+  userMainDescription:string;
+  followedUsersIdArray:string[];
+  followedCompaniesIdArray:string[]
+  userPublicationsIdArray:string[];
+  userConversationsIdArray:string[];
+}
+
+export interface companyAccount{
+  companyAccountId:string;
+  companyUserDataArray:companyUserDataElement[];
+  businessName:string;
+  TIN:string;
+  numberOfEmployees:string;
+  legalName:string;
+  companyAddress:string;
+  companyHeadquartersCountry:string;
+  companyCountriesArray:string[];
+  companyContactPhone:string;
+  companyLinkedinLink:string;
+  companyWebsiteLink:string;
+  companyMainDescription:string;
+  companyFollowedUsersIdArray:string[];
+  companyFollowedCompaniesIdArray:string[]
+  companyPublicationsIdArray:string[];
+  companyConversationsIdArray:string[];
+}
+
+export interface companyUserDataElement{
+  userId:string;
+  userRolesArray:string[];
+}
